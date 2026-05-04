728cbb98f42862ef1ea2eddfc914c9b6f56f94af
Re_Technology_ady <quanwei981@gmail.com>
2026-05-04 15:19:54 +0800

    Updata

---
+++ 二叉树/104/test.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // 终止条件：节点为空，深度为0
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+
+/*  
+
+当前节点深度 = max (左子树深度，右子树深度) + 1 
+
+    */
